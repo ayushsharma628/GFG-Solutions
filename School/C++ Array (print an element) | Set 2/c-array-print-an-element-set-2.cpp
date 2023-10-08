5b65572dbe926e0af586b3267c3ce9042ecca464
ayushsharma628 <142137234+ayushsharma628@users.noreply.github.com>
2023-10-08 15:16:28 +0530

    Added Solution - GfG to GitHub

---
+++ School/C++ Array (print an element) | Set 2/c-array-print-an-element-set-2.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int findElementAtIndex(int a[], int n, int key) 
+    {
+        int pr;
+        for(int i = 0; i<n; i++)
+        {
+            if(i==key)
+            {
+                pr = a[i];
+                break;
+            }
+        }
+        return pr;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, key;
+        cin >> n >> key;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution ob;
+        cout << ob.findElementAtIndex(a, n, key) << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
