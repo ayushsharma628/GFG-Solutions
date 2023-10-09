60929b84f477af95cf40edbe4652e2263875df0c
ayushsharma628 <142137234+ayushsharma628@users.noreply.github.com>
2023-10-09 19:53:26 +0530

    Added Solution - GfG to GitHub

---
+++ School/Even Odd Sum/even-odd-sum.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> EvenOddSum(int N, int Arr[]) {
+        int even=0, odd=0;
+        for(int i =0; i<N; i++)
+        {
+            if(i%2==0)
+            {
+                even+=Arr[i];
+            }
+            else
+            {
+                odd+=Arr[i];
+            }
+        }
+        return {even,odd};
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        int Arr[N];
+        for (int i = 0; i < N; i++) cin >> Arr[i];
+        Solution ob;
+        vector<int> ans = ob.EvenOddSum(N, Arr);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+}
+// } Driver Code Ends
